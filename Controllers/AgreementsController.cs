b5b86d056dfc0b20c305156cd2642d0e6a7874ad
Ospina Lopez, Gustavo Adolfo, Enel Colombia Extern <gaospina@indracompany.com>
2021-02-27 18:02:51 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/AgreementsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RazorAgreements.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RazorAgreements.Controllers
+{
+    public class AgreementsController: Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<AgreementsModel> agreementsList = new List<AgreementsModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44328/Agreements"))
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    agreementsList = JsonConvert.DeserializeObject<List<AgreementsModel>>(result);
+                }
+            }
+            return View(agreementsList);
+        }
+    }
+}
